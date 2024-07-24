5f591ccd9e02fd085a4f1beddb0a1a8887d6922d
Gnoyh <4ksenhs@gmail.com>
2024-07-24 22:20:27 +0900

    change

--- Docker/django/maps/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class EventFilterForm(forms.Form):
-    CATEGORY_CHOICES = [
-        ('', 'All Categories'),
-        ('sports', 'Sports'),
-    ]
-    category = forms.ChoiceField(choices=CATEGORY_CHOICES, required=False)
-    city = forms.CharField(max_length=255, required=False)
