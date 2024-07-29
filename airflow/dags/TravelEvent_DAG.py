72c8029d7426cefb9dccdbc82693754a03afb721
Gnoyh <4ksenhs@gmail.com>
2024-07-29 14:12:11 +0900

    pull develop

--- airflow/dags/TravelEvent_DAG.py
+++ airflow/dags/TravelEvent_DAG.py
@@ -8,7 +8,7 @@ import pandas as pd
 import requests
 import pytz
 
-countrys=['AE', 'AT', 'AU', 'AZ', 'BE', 'BN', 'BR', 'CA', 'CH', 'CL', 'CN', 'CZ', 'DE', 'ES', 'ET', 'FI', 'FR', 'GB', 'HR', 'HU', 'ID', 'IN', 'IT', 'JP', 'KG', 'KH', 'KZ', 'LK', 'LU', 'MN', 'MX', 'MY', 'NL', 'NO', 'NP', 'NZ', 'PE', 'PH', 'PL', 'QA', 'SA', 'SE', 'SG', 'TH', 'TR', 'TM', 'TW', 'US', 'UZ', 'VN']
+countrys=['AT', 'AU', 'BR', 'CA', 'CN', 'DE', 'ES', 'FR', 'GB', 'ID', 'IN', 'IT', 'JP', 'MY', 'NL', 'TW', 'US']
 categories = [
     "expos",
     "concerts",
@@ -44,28 +44,28 @@ def fetch_data_setting(country, category):
     data = response.json()
     return data
 def fetch_and_upload_data():
+    combined_df = pd.DataFrame()
     for country in countrys:
-        combined_df = pd.DataFrame()
         for category in categories:
             fetch_data = fetch_data_setting(country, category)
             df = pd.DataFrame(fetch_data["results"])
-            combined_df = pd.concat([combined_df, df], ignore_index=True)
-        if not combined_df.empty:
-            combined_df = combined_df.sort_values(by=['rank', 'predicted_event_spend'], ascending=[False, False])
-            combined_df.to_csv(f'/tmp/{country}_TravelEvent_data.csv', index=False, encoding='utf-8-sig')
-        else:
-            combined_df.to_csv(f'/tmp/{country}_TravelEvent_data.csv', index=False, encoding='utf-8-sig')
-            print(f"No data fetched for {country}. Skipping CSV creation.")
+            if not df.empty:
+                combined_df = pd.concat([combined_df, df], ignore_index=True)
+    if not combined_df.empty:
+        combined_df = combined_df.sort_values(by=['rank', 'predicted_event_spend'], ascending=[False, False])
+        combined_df.to_csv(f'/tmp/TravelEvent_data.csv', index=False, encoding='utf-8-sig')
+    else:
+        combined_df.to_csv(f'/tmp/TravelEvent_data.csv', index=False, encoding='utf-8-sig')
+        print(f"No data fetched for. Skipping CSV creation.")
     print("Domestic data fetched and saved to '/tmp/TravelEvents_data.csv'")
 
 def generate_and_save_data(**kwargs):
-    for country in countrys:
-        csv_filename = f'/tmp/{country}_TravelEvent_data.csv'  # Connection ID of your S3 connection in Airflow
-        s3_bucket_name = Variable.get('my_s3_bucket')
-        s3_key = f'source/source_TravelEvents/{country}_TravelEvents_data/{today}/{country}_TravelEvents_{today}.csv'
+    csv_filename = f'/tmp/TravelEvent_data.csv'  # Connection ID of your S3 connection in Airflow
+    s3_bucket_name = Variable.get('my_s3_bucket')
+    s3_key = f'source/source_TravelEvents/TravelEvents.csv'
 
-        s3_hook = S3Hook('TravelEvent_s3_conn')
-        s3_hook.load_file(filename=csv_filename, key=s3_key, bucket_name=s3_bucket_name, replace=True)
+    s3_hook = S3Hook('TravelEvent_s3_conn')
+    s3_hook.load_file(filename=csv_filename, key=s3_key, bucket_name=s3_bucket_name, replace=True)
 
 
 default_args = {
@@ -77,10 +77,10 @@ default_args = {
 
 # Define the DAG
 dag = DAG(
-    'update_JP_TravelEvents_Dags',
+    'update_TravelEvents_Dags',
     default_args=default_args,
     description='A DAG to update parking data every days and save it to S3',
-    schedule_interval='* */2 * * *',
+    schedule_interval='@daily',
     catchup=False,
 )
 
