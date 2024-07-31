702f365deda6b1a44ac475ccd329e20a99bc16d4
201810773 <sjo5525@naver.com>
2024-07-31 13:37:16 +0900

    Add Django TravelEvent Model

--- django/maps/urls.py
+++ django/maps/urls.py
@@ -5,6 +5,7 @@ from .views import upload_csv
 urlpatterns = [
     path('google-map/', views.google_map_view, name='google_map_view'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/<str:country>/', views.country, name='country_Event'),
     path('upload_csv/', upload_csv, name='upload_csv'),
     re_path(r'^event/(?P<name>.+)/(?P<location>.+)/(?P<date>.+)/(?P<category>.+)/(?P<city>.+)/$', views.event_details_view, name='event_details'),
 ]
