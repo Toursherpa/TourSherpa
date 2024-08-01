fc694ee6aa73acfc8a2eca594b4620f0d145cc4d
201810773 <sjo5525@naver.com>
2024-08-01 14:53:15 +0900

    Add eventdetail page

--- django/maps/models.py
+++ django/maps/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+import json
+
 
 class Event(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     date = models.DateTimeField()
     category = models.CharField(max_length=255)
@@ -11,4 +14,24 @@ class Event(models.Model):
     def __str__(self):
         return self.name
 
-# Create your models here.
+class TravelEvent(models.Model):
+    EventID = models.CharField(max_length=255, primary_key=True, default='none')
+    Title = models.CharField(max_length=1000, null=True, blank=True, default='none')
+    Description = models.CharField(max_length=5000, blank=True, default='none')
+    Category = models.CharField(max_length=255, null=True, blank=True, default='none')
+    Rank = models.IntegerField(null=True, blank=True, default=0)
+    PhqAttendance = models.IntegerField(null=True, blank=True, default=0)
+    TimeStart = models.CharField(max_length=50, blank=True, default='none')
+    TimeEnd = models.CharField(max_length=50, blank=True, default='none')
+    LocationID = models.CharField(max_length=50, blank=True, default='none') # Redshift의 JSONB에 매핑
+    Address = models.TextField(null=True, blank=True, default='none')
+    Region = models.CharField(max_length=255, null=True, blank=True, default='none')
+    Country = models.CharField(max_length=50, null=True, blank=True, default='none')
+    PredictedEventSpend = models.FloatField(null=True, blank=True, default=0)
+
+    class Meta:
+        db_table = 'travel_events'
+
+    def __str__(self):
+        return self.title or self.event_id
+
