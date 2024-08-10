e4b3c6638d8f60df1c20d7d326e274f8ed68d94b
snowmuffin <77647785+snowmuffin@users.noreply.github.com>
2024-08-10 19:42:53 +0900

    최적화 및 업데이트 dag추가

--- airflow/dags/hotel/Udate_full_list_DB .py
+++ airflow/dags/hotel/Udate_full_list_DB .py
@@ -5,44 +5,9 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime, timedelta
 from airflow.hooks.postgres_hook import PostgresHook
 import logging
+import os
 
 # 기본 DAG 설정
-def create_schema_table(**kwargs):
-    redshift_conn_id = 'redshift_connection'
-    table_name = 'hotel_list'
-    schema_name = 'hotel'
-    
-    redshift_hook = PostgresHook(postgres_conn_id=redshift_conn_id)
-    conn = redshift_hook.get_conn()
-    cursor = conn.cursor()
-    drop_table = f"DROP TABLE IF EXISTS {schema_name}.{table_name};"
-    
-    # Define the table schema
-    create_table_sql = f"""
-    CREATE TABLE IF NOT EXISTS {schema_name}.{table_name} (
-        event_id VARCHAR(512) PRIMARY KEY,  -- Increased length
-        google_name VARCHAR(1000),
-        google_rating FLOAT,
-        google_user_ratings_total INT,
-        google_place_id VARCHAR(512),
-        agoda_hotel_id VARCHAR(512),
-        agoda_chain_id VARCHAR(512),
-        agoda_chain_name VARCHAR(1000),
-        agoda_hotel_name VARCHAR(1000),
-        agoda_city VARCHAR(512),
-        agoda_star_rating FLOAT,
-        agoda_longitude FLOAT,
-        agoda_latitude FLOAT,
-        agoda_checkin VARCHAR(100),
-        agoda_checkout VARCHAR(100),
-        google_number_of_reviews INT
-    );
-    """
-    cursor.execute(drop_table)
-    cursor.execute(create_table_sql)
-    conn.commit()
-    cursor.close()
-    conn.close()
 
 def update_table(**kwargs):
     redshift_conn_id = 'redshift_connection'
@@ -50,16 +15,19 @@ def update_table(**kwargs):
     redshift_hook = PostgresHook(postgres_conn_id=redshift_conn_id)
     conn = redshift_hook.get_conn()
     cursor = conn.cursor()
+    
+    # 파일 경로 설정
+    file_path = f'/tmp/{datetime.utcnow().strftime("%Y-%m-%d")}/Updated_hotels.csv'
 
-    hotel_df = pd.read_csv('/tmp/Updated_hotels.csv', usecols=[
+    # 파일이 존재하는지 확인
+    try:
+        hotel_df = pd.read_csv(file_path, usecols=[
         'hotel_id', 'chain_id', 'chain_name', 'hotel_name', 'city', 'star_rating', 
         'longitude', 'latitude', 'checkin', 'checkout', 'number_of_reviews', 
         'rating', 'name', 'user_ratings_total', 'place_id'
-    ])
+        ])
 
-    hotel_df['hotel_id'] = hotel_df['hotel_id'].astype(str)
-
-    try:
+        hotel_df['hotel_id'] = hotel_df['hotel_id'].astype(str)
         insert_values = []
         for index, row in hotel_df.iterrows():
             cursor.execute(f"SELECT COUNT(*) FROM {table_name} WHERE event_id = %s", (row['hotel_id'],))
@@ -83,6 +51,7 @@ def update_table(**kwargs):
     finally:
         cursor.close()
         conn.close()
+
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 8, 2),
@@ -91,18 +60,11 @@ default_args = {
 }
 
 dag = DAG(
-    'upload_hotel_Redshift',
+    'update_hotel_Redshift',
     default_args=default_args,
     description='A DAG to update Travel Events data and save it to Redshift',
     schedule_interval='@daily',
     catchup=False,
-)
-
-upload_to_Redshift_task = PythonOperator(
-    task_id='create_schema_table',
-    python_callable=create_schema_table,
-    provide_context=True,
-    dag=dag,
 )
 
 update_Redshift_task = PythonOperator(
@@ -112,4 +74,4 @@ update_Redshift_task = PythonOperator(
     dag=dag,
 )
 
-upload_to_Redshift_task >> update_Redshift_task
+update_Redshift_task
