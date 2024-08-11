b471f9773e860ef74eb4870674fb24fa58290ece
201810773서정연 <64679164+201810773@users.noreply.github.com>
2024-08-11 23:54:11 +0900

    Update urls.py

--- django/maps/urls.py
+++ django/maps/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('dashboard/<str:country>/<str:event_id>/', views.event_detail, name='event_detail'),
     path('hotel/<str:hotel_name>/', views.hotel_detail, name='hotel_detail'),
     path('dashboard/<str:country>/', views.country, name='country_Event'),
+    path('search/', views.search_results, name='search_results'),
     path('check_availability/', views.check_availability, name='check_availability'),  # 추가된 경로
 ]
 
