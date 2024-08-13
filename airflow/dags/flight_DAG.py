9707e4d60ed7c74c9fe7b7e436fb9cee3c787458
snowmuffin <77647785+snowmuffin@users.noreply.github.com>
2024-08-13 14:05:56 +0900

    .

--- airflow/dags/flight_DAG.py
+++ airflow/dags/flight_DAG.py
@@ -9,20 +9,16 @@ import pytz
 import time
 from amadeus import Client, ResponseError
 from airflow.models import Variable
-import logging
 
 def read_csv_from_s3(bucket_name, file_key):
-    logging.info("S3에서 CSV 파일을 읽는 중...")
     s3_hook = S3Hook(aws_conn_id='s3_connection')
 
     file_obj = s3_hook.get_key(key=file_key, bucket_name=bucket_name)
     file_content = file_obj.get()['Body'].read().decode('utf-8')
 
-    logging.info("CSV 파일 읽기 완료")
     return pd.read_csv(StringIO(file_content))
 
 def fetch_flight_date(startdelta, enddelta, today):
-    logging.info("항공편 날짜 데이터를 가져오는 중...")
     s3_hook = S3Hook(aws_conn_id='s3_connection')
     bucket_name = Variable.get('s3_bucket_name')
     file_key = 'source/source_flight/nearest_airports.csv'
@@ -54,11 +50,9 @@ def fetch_flight_date(startdelta, enddelta, today):
             date = start_date + timedelta(days=i)
             airport_dict[airport_code].add(date.strftime('%Y-%m-%d'))
 
-    logging.info("항공편 날짜 데이터 가져오기 완료")
     return airport_dict
 
 def fetch_airline_data():
-    logging.info("항공사 데이터를 가져오는 중...")
     url = "https://www.airport.kr/ap/ko/dep/apAirlinesList.do"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
@@ -79,22 +73,18 @@ def fetch_airline_data():
     airline_list.append({'airline_code': 'AZ', 'airline_name': '알리탈리아'})
     airline_list.append({'airline_code': 'VS', 'airline_name': '버진애틀랜틱항공'})
 
-    logging.info("항공사 데이터 가져오기 완료")
     return pd.DataFrame(airline_list)
 
 def euro_data():
-    logging.info("유로 환율 데이터를 가져오는 중...")
     url = "https://m.stock.naver.com/marketindex/exchange/FX_EURKRW"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
 
     euro = float(soup.find_all("strong")[1].text[: -3].replace(",", ""))
 
-    logging.info(f"유로 환율: {euro}")
     return euro
 
 def fetch_flight_data(airport_dict, airline_df, euro, check):
-    logging.info("항공편 데이터를 Amadeus API에서 가져오는 중...")
     amadeus = Client(
         client_id = Variable.get("amadeus_id"),
         client_secret = Variable.get("amadeus_secret")
@@ -134,7 +124,7 @@ def fetch_flight_data(airport_dict, airline_df, euro, check):
 
                     break
                 except ResponseError as error:
-                    logging.error(f"Amadeus API 오류: {error}")
+                    print(error)
 
                     time.sleep(2)
 
@@ -156,6 +146,7 @@ def fetch_flight_data(airport_dict, airline_df, euro, check):
             flight_list.append(info_dict)
 
     flight_df = pd.DataFrame(flight_list)
+
     df = pd.merge(flight_df, airline_df, on='airline_code', how='inner')
 
     def date_change(value):
@@ -179,11 +170,9 @@ def fetch_flight_data(airport_dict, airline_df, euro, check):
     df['departure_min'] = departure_min
     df['price'] = price_won
 
-    logging.info("항공편 데이터 처리 완료")
     return df
 
 def fetch_airport_data():
-    logging.info("공항 데이터를 가져오는 중...")
     def get_lat_lng(address):
         base_url = "https://maps.googleapis.com/maps/api/geocode/json"
         params = {
@@ -199,7 +188,41 @@ def fetch_airport_data():
         return None
 
     airport_list = [
-        # ... (공항 리스트는 생략했습니다)
+        {'airport_code': 'NRT', 'airport_name': '나리타 국제공항', 'airport_location': [], 'country_code': 'JP', 'country_name': '일본'},
+        {'airport_code': 'KIX', 'airport_name': '간사이 국제공항', 'airport_location': [], 'country_code': 'JP', 'country_name': '일본'},
+        {'airport_code': 'NGO', 'airport_name': '츄부국제공항', 'airport_location': [], 'country_code': 'JP', 'country_name': '일본'},
+        {'airport_code': 'FUK', 'airport_name': '후쿠오카 공항', 'airport_location': [], 'country_code': 'JP', 'country_name': '일본'},
+        {'airport_code': 'CTS', 'airport_name': '신치토세 공항', 'airport_location': [], 'country_code': 'JP', 'country_name': '일본'},
+        {'airport_code': 'OKA', 'airport_name': '나하 공항', 'airport_location': [], 'country_code': 'JP', 'country_name': '일본'},
+        {'airport_code': 'PEK', 'airport_name': '베이징 서우두 국제공항', 'airport_location': [], 'country_code': 'CN', 'country_name': '중국'},
+        {'airport_code': 'PVG', 'airport_name': '상하이 푸둥 국제공항', 'airport_location': [], 'country_code': 'CN', 'country_name': '중국'},
+        {'airport_code': 'CAN', 'airport_name': '광저우 바이윈 국제공항', 'airport_location': [], 'country_code': 'CN', 'country_name': '중국'},
+        {'airport_code': 'CKG', 'airport_name': '충칭 장베이 국제공항', 'airport_location': [], 'country_code': 'CN', 'country_name': '중국'},
+        {'airport_code': 'HRB', 'airport_name': '하얼빈 타이핑 국제공항', 'airport_location': [], 'country_code': 'CN', 'country_name': '중국'},
+        {'airport_code': 'HKG', 'airport_name': '홍콩 국제 공항', 'airport_location': [], 'country_code': 'CN', 'country_name': '중국'},
+        {'airport_code': 'CGK', 'airport_name': '수카르노 하타 국제공항', 'airport_location': [], 'country_code': 'ID', 'country_name': '인도네시아'},
+        {'airport_code': 'DEL', 'airport_name': '인디라 간디 국제공항', 'airport_location': [], 'country_code': 'IN', 'country_name': '인도'},
+        {'airport_code': 'TPE', 'airport_name': '타이완 타오위안 국제공항', 'airport_location': [], 'country_code': 'TW', 'country_name': '대만'},
+        {'airport_code': 'KHH', 'airport_name': '가오슝 국제공항', 'airport_location': [], 'country_code': 'TW', 'country_name': '대만'},
+        {'airport_code': 'KUL', 'airport_name': '쿠알라룸푸르 국제공항', 'airport_location': [], 'country_code': 'MY', 'country_name': '말레이시아'},
+        {'airport_code': 'BKI', 'airport_name': '코타키나발루 국제공항', 'airport_location': [], 'country_code': 'MY', 'country_name': '말레이시아'},
+        {'airport_code': 'FRA', 'airport_name': '프랑크푸르트 암마인 공항', 'airport_location': [], 'country_code': 'DE', 'country_name': '독일'},
+        {'airport_code': 'MAD', 'airport_name': '마드리드 바라하스 국제공항', 'airport_location': [], 'country_code': 'ES', 'country_name': '스페인'},
+        {'airport_code': 'CDG', 'airport_name': '파리 샤를드골 국제공항', 'airport_location': [], 'country_code': 'FR', 'country_name': '프랑스'},
+        {'airport_code': 'LHR', 'airport_name': '히스로 공항', 'airport_location': [], 'country_code': 'GB', 'country_name': '영국'},
+        {'airport_code': 'FCO', 'airport_name': '로마 피우미치노 레오나르도 다 빈치 공항', 'airport_location': [], 'country_code': 'IT', 'country_name': '이탈리아'},
+        {'airport_code': 'VIE', 'airport_name': '빈 국제공항', 'airport_location': [], 'country_code': 'AT', 'country_name': '오스트리아'},
+        {'airport_code': 'AMS', 'airport_name': '암스테르담 스키폴 국제공항', 'airport_location': [], 'country_code': 'NL', 'country_name': '네덜란드'},
+        {'airport_code': 'HNL', 'airport_name': '호놀룰루 국제공항', 'airport_location': [], 'country_code': 'US', 'country_name': '미국'},
+        {'airport_code': 'SEA', 'airport_name': '시애틀 터코마 국제공항', 'airport_location': [], 'country_code': 'US', 'country_name': '미국'},
+        {'airport_code': 'LAX', 'airport_name': '로스앤젤레스 국제공항', 'airport_location': [], 'country_code': 'US', 'country_name': '미국'},
+        {'airport_code': 'ORD', 'airport_name': '시카고 오헤어 국제공항', 'airport_location': [], 'country_code': 'US', 'country_name': '미국'},
+        {'airport_code': 'DFW', 'airport_name': '달라스 / 포트워스 국제공항', 'airport_location': [], 'country_code': 'US', 'country_name': '미국'},
+        {'airport_code': 'JFK', 'airport_name': '존 F. 케네디 국제공항', 'airport_location': [], 'country_code': 'US', 'country_name': '미국'},
+        {'airport_code': 'SYD', 'airport_name': '시드니 인터내셔널 에어포트', 'airport_location': [], 'country_code': 'AU', 'country_name': '호주'},
+        {'airport_code': 'BNE', 'airport_name': '브리즈번 공항', 'airport_location': [], 'country_code': 'AU', 'country_name': '호주'},
+        {'airport_code': 'YVR', 'airport_name': '밴쿠버 국제공항', 'airport_location': [], 'country_code': 'CA', 'country_name': '캐나다'},
+        {'airport_code': 'YYZ', 'airport_name': '토론토 피어슨 국제공항', 'airport_location': [], 'country_code': 'CA', 'country_name': '캐나다'}
     ]
 
     for i, v in enumerate(airport_list):
@@ -208,11 +231,9 @@ def fetch_airport_data():
         lat, lng = get_lat_lng(address)
         airport_list[i]['airport_location'] = [lat, lng]
 
-    logging.info("공항 데이터 가져오기 완료")
     return pd.DataFrame(airport_list)
 
 def upload_to_s3(df, filename):
-    logging.info(f"S3에 {filename} 데이터를 업로드하는 중...")
     csv_filename = f'/tmp/{filename}.csv'
     df.to_csv(csv_filename, index=False, encoding='utf-8-sig')
 
@@ -220,5 +241,3 @@ def upload_to_s3(df, filename):
     s3_bucket_name = Variable.get('s3_bucket_name')
     s3_result_key = f'source/source_flight/{filename}.csv'
     s3_hook.load_file(filename=csv_filename, key=s3_result_key, bucket_name=s3_bucket_name, replace=True)
-
-    logging.info(f"S3에 {filename} 데이터 업로드 완료")
