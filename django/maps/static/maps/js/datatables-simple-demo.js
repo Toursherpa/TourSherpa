67d4b03f586579a01248bcb56940b452bb8fee6e
201810773서정연 <64679164+201810773@users.noreply.github.com>
2024-08-17 19:18:58 +0900

    주석정리

--- django/maps/static/maps/js/datatables-simple-demo.js
+++ django/maps/static/maps/js/datatables-simple-demo.js
@@ -1,3 +1,4 @@
+//datatablesSimple class를 가진 table은 simpleDatatables을 만드는 함수
 window.addEventListener('DOMContentLoaded', event => {
     const datatableElements = document.querySelectorAll('table.datatablesSimple');
 
