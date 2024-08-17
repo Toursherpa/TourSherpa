41feab90eb9d412adbad40f6ab6ef624380a551c
201810773서정연 <64679164+201810773@users.noreply.github.com>
2024-08-17 19:10:14 +0900

    주석정리

--- django/maps/templatetags/custom_filters.py
+++ django/maps/templatetags/custom_filters.py
@@ -7,6 +7,7 @@ register = template.Library()
 # ISO 8601 날짜 형식을 위한 정규 표현식
 ISO_8601_REGEX = re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$')
 
+#데이터가 문자열일 경우 정규표준식으로 데이터 형식 변환
 @register.filter
 def format_date_str(date_str):
     if ISO_8601_REGEX.match(date_str):
@@ -14,9 +15,10 @@ def format_date_str(date_str):
             dt = datetime.fromisoformat(date_str)
             return dt.strftime('%Y-%m-%d %H시 %M분')
         except ValueError:
-            return date_str  # 변환 실패 시 원문 반환
+            return date_str  
     return date_str
 
+#데이터가 datetime 형식일 경우 데이터 형식 변환
 @register.filter
 def format_date(date_str):
     dt = datetime.fromisoformat(date_str)
